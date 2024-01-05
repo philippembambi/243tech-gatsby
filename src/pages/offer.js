37cf1963a2337d7be8f176d86452ad2f53284d47
philippembambi <philippembambi413@gmail.com>
2024-01-05 17:45:46 +0100

    merge main with last changes that multiple page for each sections on the site

--- src/pages/offer.js
+++ src/pages/offer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "../components/layout";
-import { StaticImage } from "gatsby-plugin-image";
 import TopHeader from "../components/header";
 import Menu from "../components/menu";
 import Footer from "../components/footer";
